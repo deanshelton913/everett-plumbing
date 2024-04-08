eedd7eabbdb0c1544a83d49afcaa3e1d05222fb1
dean shelton <deanshelton913@gmail.com>
2024-04-08 12:41:20 -0700

    analytics

--- apps/lead-generator-ui/src/app/layout.tsx
+++ apps/lead-generator-ui/src/app/layout.tsx
@@ -2,12 +2,13 @@ import * as React from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { Analytics } from "@vercel/analytics/react"
 import theme from '@/theme';
-
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Analytics/>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
